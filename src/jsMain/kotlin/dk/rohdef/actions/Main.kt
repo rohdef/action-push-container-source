f240352e3f5dd9dac194e8aa15cd7858075957c0
Rohde Fischer <rohdef@rohdef.dk>
2025-03-04 16:49:07 +0100

    Updated with auto tagging

--- src/jsMain/kotlin/dk/rohdef/actions/Main.kt
+++ src/jsMain/kotlin/dk/rohdef/actions/Main.kt
@@ -37,12 +37,14 @@ class ActionEnvironment(
 
     val ACTOR = "GITHUB_ACTOR"
     val COMMIT_SHA = "GITHUB_SHA"
+    val REFERENCE = "GITHUB_REF"
     val REPOSITORY = "GITHUB_REPOSITORY"
     val RUN_ID = "GITHUB_RUN_ID"
     val SERVER_URL = "GITHUB_SERVER_URL"
 
     val actor = process.getEnvironmentValue(ACTOR)
     val commitSha = process.getEnvironmentValue(COMMIT_SHA)
+    val reference = process.getEnvironmentValue(REFERENCE)
     val repository = process.getEnvironmentValue(REPOSITORY)
     val runId = process.getEnvironmentValue(RUN_ID)
     val serverUrl = process.getEnvironmentValue(SERVER_URL)
@@ -60,44 +62,55 @@ suspend fun main() {
             info("Starting push workflow with the following input:\n$inputs")
 
             val autoTags: List<String> = when (inputs.autoTagging.strategy) {
-                Inputs.AutoTagging.Strategy.TAGS_AS_RELEASE -> listOf()
+                Inputs.AutoTagging.Strategy.TAGS_AS_RELEASE -> {
+                    val referenceParts = actionEnvironment.reference.split("/")
+                    val lastReferencePart = referenceParts.last()
+
+                    val version = when {
+                        referenceParts.subList(0, 2) == listOf("refs", "tags") -> lastReferencePart.replace("^v".toRegex(), "")
+                        lastReferencePart == "main" -> "latest"
+                        else -> lastReferencePart
+                    }
+
+                    listOf(version)
+                }
                 Inputs.AutoTagging.Strategy.DISABLED -> emptyList()
             }
 
             val tags = autoTags + inputs.destinationTags.tags
 
-            val taggedImages = inputs.destinationImageNames.names.flatMap { image ->
+            val imageIds = inputs.destinationImageNames.names.flatMap { image ->
                 tags.map { tag -> "$image:$tag" }
             }
 
-            info("The following images with tags will be pushed: ${taggedImages.joinToString(", ")}")
-            val imagesToPush = mutableListOf<String>()
+            info("The following images with tags will be pushed: ${imageIds.joinToString(", ")}")
+            val imageIdsToPush = mutableListOf<String>()
             inputs.destinationHosts.hosts.forEach { host ->
                 info("\tPusing images with tags to [$host]")
 
-                taggedImages.forEach { imageAndTag ->
-                    val destination = "$host/$imageAndTag"
+                imageIds.forEach { imageAndTag ->
+                    val imageId = "$host/$imageAndTag"
 
                     val tagExecution = Exec.getExecOutput(
                         "docker",
-                        arrayOf("tag", inputs.imageId.value, destination),
+                        arrayOf("tag", inputs.imageId.value, imageId),
                     ).await()
 
                     if (tagExecution.exitCode != 0) {
                         error("Could not add tag to image")
                         error("    image: ${inputs.imageId.value}")
-                        error("    tag: ${destination}")
-                        setFailed("Could not add tag [$destination] to image [${inputs.imageId.value}]'")
+                        error("    tag: ${imageId}")
+                        setFailed("Could not add tag [$imageId] to image [${inputs.imageId.value}]'")
                     }
 
-                    imagesToPush += destination
+                    imageIdsToPush += imageId
                 }
             }
 
             val imagesPushed =  mutableListOf<String>()
-            imagesToPush.forEach { image ->
-                info("""docker push "$image" """)
-                imagesPushed += image
+            imageIdsToPush.forEach { imageId ->
+                info("""docker push "$imageId" """)
+                imagesPushed += imageId
             }
             setOutput(OutputName.IMAGES_PUSHED, imagesPushed.map { "- $it" }.joinToString("\n"))
 
